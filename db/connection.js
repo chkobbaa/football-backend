66cdecec46fe8288eae8b10a480640a3327010e5
Rayen Bahroun <183955671+chkobbaa@users.noreply.github.com>
2025-07-10 03:13:28 +0100

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,4 +3,10 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
 module.exports = pool;
